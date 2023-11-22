21538051d2892baf3f5d8b5941cc67e18ad9b313
Wareezy <warrenjaftha16@gmail.com>
2023-11-22 11:03:26 +0200

    warrenJafha

---
+++ index.js
@@ -0,0 +1,21 @@
+
+//declaring all the variables 
+let display=document.querySelector('[data-display]')
+//that is 365.24 because we are including the leap year amount of days
+let daysInYear=365.24
+let btnMulti=document.querySelector('[data-calc]')
+
+//function that will be performing the calculation
+function convert(){
+    let input= document.querySelector('[data-input]').value
+    //backticks are used to access the variables and to display the data more neatly
+    let answer=eval(`${input} * ${daysInYear}`)
+    answer=answer.toFixed(2);
+    display.value=answer
+}
+//calling a click function. so that when the button is clicked that it accesses the convert function
+btnMulti.addEventListener('click',convert)
+
+
+//makes that so that the input is only readable
+document.querySelector("[data-display]").readOnly = true;
